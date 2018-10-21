a31e99b4128a16d6a3bcd7d7d530585c2b88c135
zhujialei <zjl@jfpal.com>
2018-10-22 00:21:57 +0800

    自定序列化协议、与protobuf对比分析

---
+++ Serial/src/main/java/com/cn/Test2.java
@@ -0,0 +1,27 @@
+package com.cn;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		int id = 101;
+		int age = 21;
+
+		// allocate必须制定大小，不会自动扩容
+		ByteBuffer buffer = ByteBuffer.allocate(8);
+		buffer.putInt(id);
+		buffer.putInt(age);
+		byte[] array = buffer.array();
+		System.out.println(Arrays.toString(buffer.array()));
+		
+		//====================================================
+		
+		ByteBuffer buffer2 = ByteBuffer.wrap(array);
+		System.out.println("id:"+buffer2.getInt());
+		System.out.println("age:"+buffer2.getInt());
+
+	}
+
+}
